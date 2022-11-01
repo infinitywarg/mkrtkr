adbeb6815600e697ceb07f58d205770ee8ecf934
infinitywarg <ganesh.k.anantwar@gmail.com>
2022-11-01 18:10:40 +0530

    pool logic initial commit

--- contracts/protocol/Pool.sol
+++ contracts/protocol/Pool.sol
@@ -2,9 +2,12 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "../interfaces/IPosition.sol";
 import "../libraries/SmartToken.sol";
 
 contract Pool is Ownable {
+  address public position;
+
   struct EventData {
     bytes32 name;
     uint256 start;
@@ -14,7 +17,12 @@ contract Pool is Ownable {
   }
 
   mapping(uint128 => EventData) public events;
-  mapping(uint128 => bool) public eventExists;
+  mapping(uint128 => mapping(uint16 => uint64)) public makerPositions;
+  mapping(uint128 => mapping(uint16 => uint64)) public takerPositions;
+
+  function initialize(address _position) external onlyOwner {
+    position = _position;
+  }
 
   function create(
     bytes32 name,
@@ -25,7 +33,6 @@ contract Pool is Ownable {
     EventData memory e = EventData(name, start, end, false, false);
     uint128 id = uint128(bytes16(keccak256(abi.encode(e))));
     events[id] = e;
-    eventExists[id] = true;
   }
 
   function complete(uint128 id, bool result) external onlyOwner {
@@ -45,7 +52,32 @@ contract Pool is Ownable {
     require(events[data.eventId].end != 0, "Invalid Event");
   }
 
-  function make() external {}
+  function make(
+    uint128 eventId,
+    uint16 odds,
+    uint64 value
+  ) external {
+    uint256 makerId = tokenId(SmartToken.Metadata(eventId, 0, odds, value));
+    uint256 takerId = tokenId(SmartToken.Metadata(eventId, 1, odds, value));
+    // validate if sender has enough collateral
+    makerPositions[eventId][odds] += (odds * value) / 1e2;
+    IPosition(position).mint(msg.sender, makerId, value, "");
+    IPosition(position).mint(address(this), takerId, value, "");
+  }
 
-  function take() external {}
+  function take(
+    uint128 eventId,
+    uint16 odds,
+    uint64 value
+  ) external {
+    uint256 makerId = tokenId(SmartToken.Metadata(eventId, 0, odds, value));
+    uint256 takerId = tokenId(SmartToken.Metadata(eventId, 1, odds, value));
+    require(IPosition(position).balanceOf(address(this), takerId) >= value, "Not enough Market");
+    takerPositions[eventId][odds] += value;
+    // validate if sender has enough collateral
+  }
+
+  // no need embded value in the tokenId, instead embded mint_timestamp
+  // update the SmartToken library
+  // to calculate the timebased claim on the pool at the time of redemption for MAKERS
 }
