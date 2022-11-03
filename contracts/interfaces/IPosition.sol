31431c7b8e701aa3739cb868265394e492d45d1e
infinitywarg <ganesh.k.anantwar@gmail.com>
2022-11-03 16:45:56 +0530

    revert to ERC1155 positions

--- contracts/interfaces/IPosition.sol
+++ contracts/interfaces/IPosition.sol
@@ -4,31 +4,15 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 
 interface IPosition is IERC1155 {
-  function supply(uint256 id) external view returns (uint256 total);
-
   function mint(
-    address to,
+    address account,
     uint256 id,
-    uint256 amount,
-    bytes memory data
-  ) external returns (bool success);
-
-  function mintBatch(
-    address to,
-    uint256[] memory ids,
-    uint256[] memory amounts,
-    bytes memory data
+    uint256 amount
   ) external returns (bool success);
 
   function burn(
-    address from,
+    address account,
     uint256 id,
     uint256 value
-  ) external returns (bool success);
-
-  function burnBatch(
-    address from,
-    uint256[] memory ids,
-    uint256[] memory values
   ) external returns (bool success);
 }
--- contracts/protocol/Position.sol
+++ contracts/protocol/Position.sol
@@ -5,51 +5,33 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "../interfaces/IPosition.sol";
 
 contract Position is IPosition, ERC1155 {
-  address public pool;
+  address public immutable pool;
   mapping(uint256 => uint256) public supply;
 
-  constructor() ERC1155("") {}
+  constructor(address _pool) ERC1155("") {
+    pool = _pool;
+  }
 
   modifier onlyPool() {
-    require(msg.sender == pool, "Caller Not Pool");
+    require(msg.sender == pool, "Caller not Pool");
     _;
   }
 
   function mint(
-    address to,
+    address account,
     uint256 id,
-    uint256 amount,
-    bytes memory data
+    uint256 amount
   ) external onlyPool returns (bool success) {
-    _mint(to, id, amount, data);
-    success = true;
-  }
-
-  function mintBatch(
-    address to,
-    uint256[] memory ids,
-    uint256[] memory amounts,
-    bytes memory data
-  ) external onlyPool returns (bool success) {
-    _mintBatch(to, ids, amounts, data);
+    _mint(account, id, amount, "");
     success = true;
   }
 
   function burn(
-    address from,
+    address account,
     uint256 id,
     uint256 value
   ) external onlyPool returns (bool success) {
-    _burn(from, id, value);
-    success = true;
-  }
-
-  function burnBatch(
-    address from,
-    uint256[] memory ids,
-    uint256[] memory values
-  ) external onlyPool returns (bool success) {
-    _burnBatch(from, ids, values);
+    _burn(account, id, value);
     success = true;
   }
 
