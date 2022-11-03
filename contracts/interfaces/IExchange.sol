feeb78077284b26d09cea1d28bdae365b932211a
infinitywarg <ganesh.k.anantwar@gmail.com>
2022-11-04 00:56:48 +0530

    add ERC1155receiver interface

--- contracts/interfaces/IExchange.sol
+++ contracts/interfaces/IExchange.sol
@@ -1,4 +1,29 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.9;
 
-interface IExchange {}
+import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
+
+interface IExchange is IERC1155Receiver {
+  function startGame(
+    bytes4 team1,
+    bytes4 team2,
+    bytes8 series,
+    uint32 startTime
+  ) external;
+
+  function endGame(uint64 gameId, bool result) external;
+
+  function make(
+    uint16 odds,
+    uint64 gameId,
+    uint128 value
+  ) external;
+
+  function take(
+    uint16 odds,
+    uint64 gameId,
+    uint128 value
+  ) external;
+
+  function redeem(uint256 tokenId, uint128 value) external;
+}
