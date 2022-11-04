a75454304c5bbed834f82bf1c5cd62d2835fcd54
infinitywarg <ganesh.k.anantwar@gmail.com>
2022-11-04 20:53:24 +0530

    contract addresses with hardhat node

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -29,6 +29,21 @@ async function main() {
 	console.log(`Initialize Exchange contract complete`);
 
 	// startGame: ["0x00494e44","0x00415553","0x0049434357433232","1667509980"]
+
+	/**
+	 * 
+	 * Compiled 1 Solidity file successfully
+Deployment started!
+Deploying contracts with: 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+Deploying Cash contract
+Cash contract deployed to 0x5FbDB2315678afecb367f032d93F642f64180aa3
+Deploying Exchange contract
+Exchange contract deployed to 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
+Deploying Coupon contract
+Coupon contract deployed to 0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0
+Initialize Exchange contract
+Initialize Exchange contract complete
+	 */
 }
 
 main().catch((error) => {
